1b59de3f78d38670f9df27e61b3bb6999be28190
rozetko <rozetko@corpglory.com>
2022-01-25 16:46:50 +0300

    Add Device: get rid of all "ng-" stuff

--- src/pages/A.tsx
+++ src/pages/A.tsx
@@ -13,7 +13,7 @@ const defaults = {
   device_flow_type: 'sflow',
   device_sample_rate: 5,
   sending_ips: '',
-  minimize_snmp: false,
+  minimize_snmp: 'false',
   device_bgp_type: 'none',
   device_snmp_ip: '',
   device_snmp_community: '',
@@ -42,13 +42,13 @@ export const A: FC<AppRootProps> = (props) => {
 
   const [state, setState] = useState({
     device: _.cloneDeep(defaults),
-    sendingIps: [{ ip: '1' }]
+    sendingIps: [{ ip: '' }]
   })
 
   function addIP(): void {
     setState({
       ...state,
-      sendingIps: _.concat(state.sendingIps, { ip: '2' })
+      sendingIps: _.concat(state.sendingIps, { ip: '' })
     });
   }
 
@@ -85,14 +85,21 @@ export const A: FC<AppRootProps> = (props) => {
     _.forEach(state.sendingIps, ip => {
       ips.push(ip.ip);
     });
-    state.device.sending_ips = ips.join();
-    const resp = await backendSrv.post(`/api/plugin-proxy/kentik-connect-app/api/v5/device`, state.device);
+    const payload = {
+      ...state.device,
+      sending_ips: ips.join()
+    };
+    const resp = await backendSrv.post(`/api/plugin-proxy/kentik-connect-app/api/v5/device`, payload);
     if ('err' in resp) {
       // this.alertSrv.set('Device Add failed.', resp.err, 'error');
       throw new Error(`Device Add failed: ${resp.err}`);
     } else {
       // this.$location.url('/plugins/kentik-connect-app/page/device-list');
     }
+  }
+
+  function cancel(): void {
+    // go to the Home dashboard
   }
 
   let typeSelector;
@@ -103,7 +110,6 @@ export const A: FC<AppRootProps> = (props) => {
         <div className="gf-form-select-wrapper">
           <select
             className="gf-form-input gf-size-auto"
-            ng-model="ctrl.device.device_flow_type"
             value={state.device.device_flow_type}
             onChange={(event) => onDeviceFieldChange(event, 'device_flow_type')}
           >
@@ -149,13 +155,23 @@ export const A: FC<AppRootProps> = (props) => {
       <div className="gf-form-group">
         <div className="gf-form">
           <label className="gf-form-label width-11">Device Name</label>
-          <input className="gf-form-input max-width-21" type="text" ng-model="ctrl.device.device_name" pattern="^[A-Za-z0-9_]{1,15}$"
-            required />
+          <input 
+            className="gf-form-input max-width-21" 
+            type="text"
+            pattern="^[A-Za-z0-9_]{1,15}$"
+            required 
+            value={state.device.device_name}
+            onChange={(event) => onDeviceFieldChange(event, 'device_name')}
+          />
         </div>
         <div className="gf-form">
           <label className="gf-form-label width-11">Device Type</label>
           <div className="gf-form-select-wrapper">
-            <select className="gf-form-input gf-size-auto" ng-model="ctrl.device.device_type">
+            <select
+              className="gf-form-input gf-size-auto"
+              value={state.device.device_type}
+              onChange={(event) => onDeviceFieldChange(event, 'device_type')}
+            >
               <option value="router">Router</option>
               <option value="host-nprobe-basic">Host</option>
             </select>
@@ -166,7 +182,12 @@ export const A: FC<AppRootProps> = (props) => {
       <div className="gf-form-group">
         <div className="gf-form">
           <label className="gf-form-label width-11">Description</label>
-          <input className="gf-form-input max-width-21" type="text" ng-model="ctrl.device.device_description" />
+          <input
+            className="gf-form-input max-width-21"
+            type="text"
+            value={state.device.device_description}
+            onChange={(event) => onDeviceFieldChange(event, 'device_description')}
+          />
         </div>
         {typeSelector}
         <div className="gf-form">
@@ -183,8 +204,13 @@ export const A: FC<AppRootProps> = (props) => {
           state.sendingIps.map((ip, index) => {
             return <div className="gf-form">
               <label className="gf-form-label width-11">Source Address</label>
-              <input className="gf-form-input max-width-15" type="text" value={ip.ip} onChange={(event) => handleIpChange(event, index)} required />
-              
+              <input
+                className="gf-form-input max-width-15"
+                type="text"
+                value={ip.ip}
+                onChange={(event) => handleIpChange(event, index)}
+                required
+              />
               {
                 index === 0 ?
                 <a className="btn btn-inverse btn-small" onClick={addIP}><i className="fa fa-plus"></i></a> :
@@ -194,35 +220,53 @@ export const A: FC<AppRootProps> = (props) => {
           })
         }
       </div>
-      <div className="gf-form-group">
-        <div className="gf-form" ng-if="ctrl.device.device_type == 'router'">
-          <label className="gf-form-label width-11">SNMP Polling</label>
-          <div className="gf-form-select-wrapper">
-            <select className="gf-form-input gf-size-auto" ng-model="ctrl.device.minimize_snmp" ng-options="i.v as i.l for i in [{v: true, l: 'Minimum'}, {v: false, l: 'Standard'}]">
-            </select>
+      { state.device.device_type === 'router' && (
+        <div className="gf-form-group">
+          <div className="gf-form">
+            <label className="gf-form-label width-11">SNMP Polling</label>
+            <div className="gf-form-select-wrapper">
+              <select
+                className="gf-form-input gf-size-auto"
+                value={state.device.minimize_snmp}
+                onChange={(event) => onDeviceFieldChange(event, 'minimize_snmp')}
+              >
+                <option value="true">Minimum</option>
+                <option value="false">Standard</option>
+              </select>
+            </div>
+          </div>
+          <div className="gf-form">
+            <label className="gf-form-label width-11">SNMP Community</label>
+            <input
+              className="gf-form-input max-width-15"
+              type="text"
+              value={state.device.device_snmp_community}
+              onChange={(event) => onDeviceFieldChange(event, 'device_snmp_community')}
+            />
+          </div>
+          <div className="gf-form">
+            <label className="gf-form-label width-11">Device IP</label>
+            <input
+              className="gf-form-input max-width-15"
+              type="text"
+              value={state.device.device_snmp_ip}
+              onChange={(event) => onDeviceFieldChange(event, 'device_snmp_ip')}
+            />
           </div>
         </div>
-        <div className="gf-form" ng-if="ctrl.device.device_type == 'router'">
-          <label className="gf-form-label width-11">SNMP Community</label>
-          <input className="gf-form-input max-width-15" type="text" ng-model="ctrl.device.device_snmp_community" />
-        </div>
-        <div className="gf-form" ng-if="ctrl.device.device_type == 'router'">
-          <label className="gf-form-label width-11">Device IP</label>
-          <input className="gf-form-input max-width-15" type="text" ng-model="ctrl.device.device_snmp_ip" />
-        </div>
-      </div>
+      )}
       <div className="gf-form-group">
         <div className="gf-form">
           <label className="gf-form-label width-11">BGP</label>
           <div className="gf-form-select-wrapper">
             <select className="gf-form-input max-width-15" disabled>
-              <option>Available with paid acounts</option>
+              <option>Available with paid accounts</option>
             </select>
           </div>
         </div>
       </div>
-      <button type="submit" className="btn btn-success" onClick={addDevice}>Add Device</button>
-      <a className="btn btn-link" ng-click="ctrl.cancel();">Cancel</a>
+      <button className="btn btn-success" onClick={addDevice}>Add Device</button>
+      <a className="btn btn-link" onClick={cancel}>Cancel</a>
     </form>
   );
 };
