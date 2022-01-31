fa6dd4c37860ad20a56a0382ddae44c07bcda34e
rozetko <rozetko@corpglory.com>
2022-01-31 17:51:43 +0300

    minor fix

--- src/utils/consts.ts
+++ src/utils/consts.ts
@@ -1,2 +1,2 @@
-export const APP_TITLE = 'Simple App Plugin';
-export const APP_SUBTITLE = 'Simple App Plugin subtitle';
+export const APP_TITLE = 'Kentik Connect Pro';
+export const APP_SUBTITLE = '';
